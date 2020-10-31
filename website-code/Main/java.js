17931455c7b64c87cdbfdf65eb197f3336cf4e90
Vuochlang <72162972+Vuochlang@users.noreply.github.com>
2020-10-30 19:17:10 -0700

    Made some changes in css

--- website-code/Main/java.js
+++ website-code/Main/java.js
@@ -8,38 +8,3 @@ function callExit(input){
     }   
     return false;  
 }
-
-    //     function drawCanvas() {
-    //         let Canvas = document.getElementById('canvas');
-    //         let context = Canvas.getContext('2d');
-            
-    //         start();
-
-    //        function start() {
-    //            window.addEventListener('resize', resizeCanvas, false);
-    //            resizeCanvas();
-    //         }
-
-    //         function resizeCanvas() {
-    //             Canvas.width = window.innerWidth;
-    //             Canvas.height = window.innerHeight;
-    //             redraw();
-    //         }
-
-    //         function redraw() {
-    //             // context.strokeStyle = '#D5F0F2';
-    //             // context.lineWidth = '10';
-    //             // context.strokeRect(0, 0, window.innerWidth, window.innerHeight);
-    //             context.fillStyle="#271F3F";
-    //             context.fillRect(5,5,window.innerWidth-10,window.innerHeight-10);
-    //             context.shadowColor='#D4F5F8';
-    //             context.shadowBlur=5;
-    //             context.strokeStyle = '#C3F6FB';
-    //             context.lineWidth = '10';
-    //             context.strokeRect(0, 0, window.innerWidth, window.innerHeight);
-
-    //             context.drawImage(logo,0,0)
-    //             context.drawImage(star1,window.innerWidth,window.innerHeight)
-    //             context.drawImage(star2,window.innerWidth,window.innerHeight)
-    //         }
-    // };
