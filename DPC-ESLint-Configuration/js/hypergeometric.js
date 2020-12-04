fa3dd1bf47471aa13d5d45d8d8e6cf95cc5aecdb
ZhichengZhou-1 <72363721+ZhichengZhou-1@users.noreply.github.com>
2020-12-04 14:43:42 -0800

    Made some changes but unfinished

--- DPC-ESLint-Configuration/js/hypergeometric.js
+++ DPC-ESLint-Configuration/js/hypergeometric.js
@@ -4,7 +4,7 @@ let validate;
 function callHypergeometric(num) {
   switch (num) {
     case 0:// on page load
-      hypergeometric = new hypergeometric();
+      hypergeometric = new Hypergeometric();
       break;
     case 1:// x
       hypergeometric.checkX();
@@ -12,10 +12,10 @@ function callHypergeometric(num) {
     case 2:// n
       hypergeometric.check_n();
       break;
-    case 3:// p
+    case 3:// N
       hypergeometric.checkN();
       break;
-    case 4:
+    case 4:// K
       hypergeometric.checkK();
       break;
     case 'reset':// reset button
@@ -36,7 +36,7 @@ class Hypergeometric {
   }
 
   calculate () {
-    if(checkAll()==true) {
+    if(this.checkAll()==true) {
       document.getElementById("result").disabled = false;
       const x = document.getElementById('x').value;
       const n = document.getElementById('n').value;
@@ -67,9 +67,9 @@ class Hypergeometric {
   }
 
   checkN() {
-    const nValue = document.getElementById('N').value;
+    const NValue = document.getElementById('N').value;
     const error = document.getElementById('error2');
-    if (validate.checkIfValidInput(nValue) === true) {
+    if (validate.checkIfValidInput(NValue) === true) {
       if (this.checkNValue() === 1) {
         error.style.color = 'green';
         error.value = '  ✔';
@@ -108,6 +108,31 @@ class Hypergeometric {
     return false;
   }
 
+  checkK() {
+    const KValue = document.getElementById('K').value;
+    const error = document.getElementById('error4');
+    if (validate.checkIfValidInput(KValue) === true) {
+      if (this.checkKValue() === 1) {
+        error.style.color = 'green';
+        error.value = '  ✔';
+        return true;
+      }
+      else if (this.checkKValue() === -2) {
+        error.style.color = 'red';
+        error.value = '  K must be greater than or equal to x';
+        return false;
+
+      } else {
+        error.style.color = 'red';
+        error.value = ' K must be less than or equal to K'
+        return false;
+      }
+    }
+    error.style.color = 'red';
+    error.value = '  Input must be valid';
+    return false;
+  }
+
   check_nValue() { // n<=x
     const x = parseFloat(document.getElementById('x').value);
     const n = parseFloat(document.getElementById('n').value);
@@ -115,7 +140,7 @@ class Hypergeometric {
   }
 
   checkNValue() {
-   // const x = parseFloat(document.getElementById('x').value);
+    // const x = parseFloat(document.getElementById('x').value);
     const n = parseFloat(document.getElementById('n').value);
     const N = parseFloat(document.getElementById('N').value);
 
@@ -123,234 +148,42 @@ class Hypergeometric {
       return 1;
     }
     else if( N>= 1){
-      return -2;
+      return -2; // N !>= n
     }
-    else return -3;
+    else return -3; // N!>= 1
   }
 
-  checkK() {
+  checkKValue() {
     const K = parseFloat(document.getElementById('K').value);
     const N = parseFloat(document.getElementById('N').value);
-    return K <= N;
-  }
-
-
-
-  }
-
-
-
-
-
-/*
-
-function calculate_hyper(x,n,N,k){
-  if(checkAll()==true) {
-    document.getElementById("result").disabled = false;
-    let temp1 = Combination(k, x);
-    let temp2 = Combination((N - k), (n - x));
-    let temp3 = Combination(N, n);
-    let temp4 = (temp1 * temp2) / temp3;
-    document.getElementById("result").value = (temp4).toFixed(15);
-  }
-  else{
-    document.getElementById("result").value="Invalid Input";
-  }
-}
-/*function Combination(first,second){
-  let numerator=factorial(first);
-  let denominator=(factorial(second))*factorial((first-second));
-  let quotient=numerator/denominator;
-  return quotient;
-}
-
-function factorial(num) {
-  if (num == 0) {
-    return 1;
-  }
-  else{
-    return (num * factorial(num - 1));
-  }
-}*/
-/*function checkAll(){
-  if(check(1)==true&&check(2)==true&&check(3)==true){
-    if(checkNValue()==true && checkNXValue()==true){
-      return true;
+    const x = parseFloat(document.getElementById('x').value);
+    if( K<= N && K >= x){
+      return 1;
     }
-  }
-  return false;
-}*/
-/*
-function check(num) {
-  if (num == 1) {
-    let xValue = document.getElementById("x").value;
-    let error = document.getElementById("error1");
-    if (checkIfValidInput(xValue) == true) {
-      error.value = "✔";
-      return true;
-    } else {
-      error.value = "Input must be valid";
-      return false;
+    else if( K<= N) {
+      return -2; // K !>= x
     }
+    else return -3; //K !<= N
   }
-  else if(num==2){
-    let nValue=document.getElementById("n").value;
-    let error=document.getElementById("error2");
-    if(checkIfValidInput(nValue)==true){
-      if(checkNXValue()==true){
-        error.value="✔";
+
+  checkAll() {
+    if (this.checkX() === true && this.checkN() === true && this.check_n() === true && this.checkK() === true) {
+      if (this.check_nValue() === true && this.checkNValue() === 1 && this.checkKValue() === 1) {
         return true;
       }
-      else{
-        error.value="n must be greater than x";
-        return false;
-      }
     }
-    else{
-      error.value="Input must be valid";
-      return false;
-    }
-  }
-  else if(num==3){ //num==3
-      let NValue=document.getElementById("N").value;
-      let kValue=document.getElementById("k").value;
-      let nValue=document.getElementById("n").value;
-
-      let error=document.getElementById("error3");
-      if(checkIfValidInput(NValue,kValue,nValue)==true){
-        if(checkNValue()==true){
-          error.value="✔";
-          return true;
-        }
-        else if(checkNValue()==1){
-          error.value="N must be >= 1";
-          return false;
-        }
-        else if(checkNValue()==2){
-            error.value="N must be >= k"
-          }
-          else if(checkNValue()==3){
-              error.value='N must be >= n'
-            }
-      }
-      else{
-        error.value="Input must be valid";
-        return false;
-      }
-    }
-}
-function checkIfValidInput(value){
-  if(checkIfValid(value)==false || checkNumberOnly(value)==false){
     return false;
   }
-  return true;
-}
 
-function checkIfValid(value){
-  if(isNaN(parseFloat(value)))	return false;
-  return true;
-}
-
-function checkNumberOnly(value){
-  let getDecimal=false;
-  for(let i=0; i<value.length; i++){
-    if(getDecimal===false){
-      if(value.charCodeAt(i)!==46 && value.charCodeAt(i)<48 || value.charCodeAt(i)>57){
-        return false;
-      }
-      if(value.charCodeAt(i)===46){
-        getDecimal=true;
-        if((i+1)===value.length) return false;
-        i++;
-      }
-    }
-    if(getDecimal===true){
-      if(value.charCodeAt(i)<48 || value.charCodeAt(i)>57){
-        return false;
-      }
-    }
-  }
-  return true;
-}
-
-function checkNXValue(){
-  let x=parseFloat(document.getElementById("x").value);
-  let n=parseFloat(document.getElementById("n").value);
-  if(n<=x){
-    return false;
-  }
-  return true;
-}
-
-
-function checkNValue() {
-  let N_one_check;
-  let N_k_check;
-  let N_n_check;
-  let N = document.getElementById("N").value;
-  let k = document.getElementById("k").value;
-  let n= document.getElementById("n").value;
-  if (N >= 1) {
-    N_one_check = true;
-  } else return 1;
-
-  if (N>=k) {
-    N_k_check = true;
-  } else return 2;
-  if (N>=n){
-    let N_n_check=true
-  } else return 3;
-  if(N_one_check==true && N_k_check==true && N_n_check==true){
-    return 4;//true value is 4, everything else is false value
+  reset() {
+    document.getElementById('x').value = '0';
+    document.getElementById('n').value = '0';
+    document.getElementById('N').value = '0';
+    document.getElementById('K').value = '0';
+    document.getElementById('error1').value = '';
+    document.getElementById('error2').value = '';
+    document.getElementById('error3').value = '';
+    document.getElementById('error4').value = '';
+    document.getElementById('result').value = '';
   }
 }
-
-function checkAll(){
-  if(check(1)==true&&check(2)==true&&check(3)==true){
-    if(checkNValue()==4 && checkNXValue()==true){
-      return true;
-    }
-  }
-  return false;
-}
-function Combination(first,second){
-  let numerator=factorial(first);
-  let denominator=(factorial(second))*factorial((first-second));
-  let quotient=numerator/denominator;
-  return quotient;
-}
-
-function factorial(num) {
-  if (num == 0) {
-    return 1;
-  }
-  else{
-    return (num * factorial(num - 1));
-  }
-}
-
-function reset(){
-  document.getElementById("x").value="0";
-  document.getElementById("n").value="0";
-  document.getElementById("p").value="0";
-  document.getElementById("error1").value="";
-  document.getElementById("error2").value="";
-  document.getElementById("error3").value="";
-  document.getElementById("result").value="";
-}
-
-let acc = document.getElementsByClassName("info");
-let i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
-*/
