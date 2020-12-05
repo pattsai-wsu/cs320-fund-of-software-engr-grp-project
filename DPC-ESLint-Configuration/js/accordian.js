2e31cd6938e2760fda60c6bae52495518407cf75
Patrick Tsai <tsai.patrick.82@gmail.com>
2020-12-05 15:15:06 -0800

    added mobile footer - changed some highlighting for the accordian

--- DPC-ESLint-Configuration/js/accordian.js
+++ DPC-ESLint-Configuration/js/accordian.js
@@ -4,7 +4,7 @@
 
   for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
+      this.classList.toggle("activePanel");
       var panel = this.nextElementSibling;
       if (panel.style.display === "block") {
         panel.style.display = "none";
