eae89ae4a36dcce33b4a8d09fb4101cb31f7b27b
ZhichengZhou-1 <zhichengzhou123@gmail.com>
2020-12-10 14:49:27 -0800

    Made files for testing hypergeometric

--- DPC-ESLint-Configuration/testing/Hypergeometric.js
+++ DPC-ESLint-Configuration/testing/Hypergeometric.js
@@ -1,6 +1,6 @@
 describe('Test for Correctness', function () {
     describe('CLASS:  Hypergeometric', function () {
-        const temp = Hypergeometric();
+        const temp = new Hypergeometric();
         describe('Function:  checkX()', function () {
             it('when x=2.63, checkX() should return false since it must be a decimal number', function () {
                 temp.x = '2.63';
@@ -8,7 +8,7 @@ describe('Test for Correctness', function () {
                 chai.expect(temp.checkX()).to.be.false;
             });
             it('when x=10, checkX() should return true', function () {
-                temp.x = '7';
+                temp.x = '10';
                 this.slow(0);
                 chai.expect(temp.checkX()).to.be.true;
             });
