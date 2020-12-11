6f01c474724ea078207a013208311067cad2c70b
ZhichengZhou-1 <zhichengzhou123@gmail.com>
2020-12-11 12:29:19 -0800

    unfinished hypergeometric.html and hypergeometric.js

--- DPC-ESLint-Configuration/js/hypergeometric.js
+++ DPC-ESLint-Configuration/js/hypergeometric.js
@@ -2,8 +2,7 @@ let hypergeometric;
 let validate;
 
 class Hypergeometric {
-
-  constructor(a, b, c) {
+  constructor(a, b, c, d) {
     validate = new ValidateInput();
     this.x = -1;
     this.n = -1;
@@ -12,6 +11,7 @@ class Hypergeometric {
     this.error1 = a;
     this.error2 = b;
     this.error3 = c;
+    this.error4 = d;
     this.result = document.getElementById('result');
   }
 
@@ -62,15 +62,17 @@ class Hypergeometric {
       case 3: // when neither is true
         this.p = -1;
         this.error3.style.color = 'red';
-        this.error3.value = ' N must be greater than or equal to 1 and greater than or equal to n'
+        this.error3.value = ' N must be greater than or equal to 1 and greater than or equal to n';
+        break;
       default: // N is valid
-        this.p = -1
+        this.p = -1;
         this.error3.style.color = 'red';
         this.error3.value = 'Input must be valid integer';
         break;
     }
   }
-  verifyk () {
+
+  verifyk() {
     switch (this.checkK()) {
       case 1: // k is good
         this.p = parseFloat(this.p);
@@ -85,9 +87,10 @@ class Hypergeometric {
       case 3: // when neither is true
         this.p = -1;
         this.error3.style.color = 'red';
-        this.error3.value = ' k must be less than or equal to n and greater or equal to x'
+        this.error3.value = ' k must be less than or equal to n and greater or equal to x';
+        break;
       default: // k is invalid
-        this.p = -1
+        this.p = -1;
         this.error3.style.color = 'red';
         this.error3.value = 'Input must be valid integer';
         break;
@@ -120,13 +123,12 @@ class Hypergeometric {
     return false;
   }
 
-  checkn() { //1 means n is good, 2 means n!>=x, 3 means n is invalid
+  checkn() { // 1 means n is good, 2 means n!>=x, 3 means n is invalid
     if (validate.checkIfValidInput(this.n) === true && validate.isDecimal === false) {
       if (this.n >= this.x) {
         return 1;
-      } else {
-        return 2;
       }
+      return 2;
     }
     return 3;
   }
@@ -135,26 +137,25 @@ class Hypergeometric {
     if (validate.checkIfValidInput(this.n) === true && validate.isDecimal === false) {
       if (this.N >= 1 && this.N >= this.n) {
         return 1;
-      } else if (this.N >= 1) {
-          return 2; // N !>= n
-        } else
-      return 3; // N!>= 1
+      } if (this.N >= 1) {
+        return 2; // N !>= n
+      } return 3; // N!>= 1
     }
     return 4;
   }
 
   checkK() {
     if (validate.checkIfValidInput(this.n) === true && validate.isDecimal === false) {
-      if( this.k <= this.N && this.k >= this.x){
+      if (this.k <= this.N && this.k >= this.x) {
         return 1;
       }
-      else if( this.k <= this.N) {
+      if (this.k <= this.N) {
         return 2; // K !>= x
       }
-      else return 3; //K !<= N
+      return 3; // K !<= N
     }
-    return 4
-    }
+    return 4;
+  }
 
   checkAll() {
     if (this.n === -1 || this.x === -1 || this.N === -1 || this.k === -1) {
@@ -162,14 +163,15 @@ class Hypergeometric {
       this.result.value = 'Invalid Input';
       return false;
     }
-  /*  if (this.n < this.x) {
-      this.error1.value = 'x must be less than or equal n';
-      this.error2.value = '!';
-      return false;
-    }*/
+    /*  if (this.n < this.x) {
+        this.error1.value = 'x must be less than or equal n';
+        this.error2.value = '!';
+        return false;
+      } */
     this.error1.value = '✔';
     this.error2.value = '✔';
     this.error3.value = '✔';
+    this.error4.value = '✔';
     return true;
   }
 
@@ -181,6 +183,7 @@ class Hypergeometric {
     this.error1.value = '';
     this.error2.value = '';
     this.error3.value = '';
+    this.error4.value = '';
     this.result.value = '';
     this.k = -1;
     this.N = -1;
@@ -188,10 +191,9 @@ class Hypergeometric {
     this.x = -1;
     this.result.disabled = true;
   }
-
 }
 
-function callHypergeometric(num, value, value2, value3) {
+function callHypergeometric(num, value, value2, value3, value4) {
   switch (num) {
     case 0:// on page load
       hypergeometric = new Hypergeometric(value, value2, value3);
@@ -211,34 +213,6 @@ function callHypergeometric(num, value, value2, value3) {
     case 4:
       hypergeometric.k = value;
       hypergeometric.verifyk();
-    case 'reset':// reset button
-      hypergeometric.reset();
-      break;
-    case 'calculate':// result button
-      hypergeometric.calculate();
-      break;
-    default:
-      break;
-  }
-}
-
-  /*
-function callHypergeometric(num) {
-  switch (num) {
-    case 0:// on page load
-      hypergeometric = new Hypergeometric();
-      break;
-    case 1:// x
-      hypergeometric.checkX();
-      break;
-    case 2:// n
-      hypergeometric.check_n();
-      break;
-    case 3:// N
-      hypergeometric.checkN();
-      break;
-    case 4:// K
-      hypergeometric.checkK();
       break;
     case 'reset':// reset button
       hypergeometric.reset();
@@ -250,165 +224,3 @@ function callHypergeometric(num) {
       break;
   }
 }
-
-
-class Hypergeometric {
-  constructor() {
-    validate = new ValidateInput();
-  }
-
-  calculate () {
-    if(this.checkAll()==true) {
-      document.getElementById("result").disabled = false;
-      const x = document.getElementById('x').value;
-      const n = document.getElementById('n').value;
-      const N = document.getElementById('_N').value;
-      const K = document.getElementById('K').value;
-      const temp1 = validate.Combination(K, x);
-      const temp2 = validate.Combination((N - K), (n - x));
-      const temp3 = validate.Combination(N, n);
-      const temp4 = (temp1 * temp2) / temp3;
-      document.getElementById("result").value = (temp4);
-    }
-    else{
-      document.getElementById("result").value="Invalid Input";
-    }
-  }
-
-  checkX() {
-    const xValue = document.getElementById('x').value;
-    const error = document.getElementById('error1');
-    if (validate.checkIfValidInput(xValue) === true) { // valid for X
-      error.style.color = 'green';
-      error.value = '  ✔';
-      return true;
-    }
-    error.style.color = 'red';
-    error.value = '  Input must be valid';
-    return false;
-  }
-
-  checkN() {
-    const NValue = document.getElementById('_N').value;
-    const error = document.getElementById('error3');
-    if (validate.checkIfValidInput(NValue) === true) {
-      if (this.checkNValue() === 1) {
-        error.style.color = 'green';
-        error.value = '  ✔';
-        return true;
-      } else if (this.checkNValue() === -2) {
-        error.style.color = 'red';
-        error.value = 'N must be greater than or equal to n';
-        return false;
-
-      } else {
-        error.style.color = 'red';
-        error.value = ' N must be greater or equal to 1'
-        return false;
-      }
-    }
-    error.style.color = 'red';
-    error.value = '  Input must be valid';
-    return false;
-  }
-
-  check_n() {
-    const nValue = document.getElementById('n').value;
-    const error = document.getElementById('error2');
-    if (validate.checkIfValidInput(nValue) === true) {
-      if (this.check_nValue() === true) {
-        error.style.color = 'green';
-        error.value = '  ✔';
-        return true;
-      }
-      else {
-        error.style.color = 'red';
-        error.value = '  n must be greater than x';
-        return false;
-      }
-    }
-    error.style.color = 'red';
-    error.value = '  Input must be valid';
-    return false;
-  }
-
-  checkK() {
-    const KValue = document.getElementById('K').value;
-    const error = document.getElementById('error4');
-    if (validate.checkIfValidInput(KValue) === true) {
-      if (this.checkKValue() === 1) {
-        error.style.color = 'green';
-        error.value = '  ✔';
-        return true;
-      }
-      else if (this.checkKValue() === -2) {
-        error.style.color = 'red';
-        error.value = '  K must be greater than or equal to x';
-        return false;
-
-      } else {
-        error.style.color = 'red';
-        error.value = ' K must be less than or equal to N'
-        return false;
-      }
-    }
-    error.style.color = 'red';
-    error.value = '  Input must be valid';
-    return false;
-  }
-
-  check_nValue() { // n<=x
-    const x = parseFloat(document.getElementById('x').value);
-    const n = parseFloat(document.getElementById('n').value);
-    return n >= x;
-  }
-
-  checkNValue() {
-    // const x = parseFloat(document.getElementById('x').value);
-    const n = parseFloat(document.getElementById('n').value);
-    const N = parseFloat(document.getElementById('_N').value);
-
-    if( N >= 1 && N >= n){
-      return 1;
-    }
-    else if( N>= 1){
-      return -2; // N !>= n
-    }
-    else return -3; // N!>= 1
-  }
-
-  checkKValue() {
-    const K = parseFloat(document.getElementById('K').value);
-    const N = parseFloat(document.getElementById('_N').value);
-    const x = parseFloat(document.getElementById('x').value);
-    if( K<= N && K >= x){
-      return 1;
-    }
-    else if( K<= N) {
-      return -2; // K !>= x
-    }
-    else return -3; //K !<= N
-  }
-
-  checkAll() {
-    if (this.checkX() === true && this.checkN() === true && this.check_n() === true && this.checkK() === true) {
-      if (this.check_nValue() === true && this.checkNValue() === 1 && this.checkKValue() === 1) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  reset() {
-    document.getElementById('x').value = '0';
-    document.getElementById('n').value = '0';
-    document.getElementById('_N').value = '0';
-    document.getElementById('K').value = '0';
-    document.getElementById('error1').value = '';
-    document.getElementById('error2').value = '';
-    document.getElementById('error3').value = '';
-    document.getElementById('error4').value = '';
-    document.getElementById('result').value = '';
-  }
-}
-*/
